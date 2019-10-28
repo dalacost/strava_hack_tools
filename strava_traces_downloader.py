eedf97d01a57312073e20aa2cda59ee5d5146a3b
dalacost <danilo@lacosox.org>
2019-10-28 17:46:59 -0300

    new login way and adding support for date time
    new login way and adding support for date time

--- strava_traces_downloader.py
+++ strava_traces_downloader.py
@@ -1,5 +1,5 @@
 import sys
-import urllib2, urllib
+import urllib3, urllib
 import json
 import os
 import shutil
@@ -9,6 +9,8 @@ import datetime
 from datetime import timedelta
 import argparse
 import requests
+import traceback
+from bs4 import BeautifulSoup
 
 # This script can reconstruct a gpx track of a Strava activity from 
 # public information. 
@@ -29,6 +31,7 @@ import requests
 STRAVA_PATH_STREAM = 'http://www.strava.com/stream/'
 STRAVA_URL_LOGIN = 'https://www.strava.com/login'
 STRAVA_URL_SESSION = 'https://www.strava.com/session'
+STRAVA_ACTIVITIES_SESSION ='https://www.strava.com/activities/'
 STRAVA_LOGGED_OUT_FINGERPRINT = 'logged-out'
 DEFAULT_OUTPUT_FILE='output.gpx'
 
@@ -41,41 +44,45 @@ FOOT_FILE="</gpx>"
 #global session var
 LOGIN_SESSION = ''
 
-def save_as_gpx(activity_id,points,elevation_points, time_points, output_file):
+def save_as_gpx(activity_id,points,elevation_points, time_points, started_unix_time, output_file):
 
-	 final_file = open(output_file, 'w')
-	 final_file.write(HEAD_FILE)
-	 final_file.write('<trk>\n')
-	 final_file.write('<name> Activity '+str(activity_id)+'</name>\n')
-	 final_file.write('<trkseg>\n')
-	 points_counter=0
+	final_file = open(output_file, 'w')
+	final_file.write(HEAD_FILE)
+	final_file.write('<trk>\n')
+	final_file.write('<name> Activity '+str(activity_id)+'</name>\n')
+	final_file.write('<trkseg>\n')
+	points_counter=0
 
-	 for point in points:
+	for point in points:
 		#not save 0,0 points, this are points inside of protection area. 
 		if not (str(point[0]) == '0.0') and not (str(point[1]) == '0.0'):
 			final_file.write('\t<trkpt lat="'+str(point[0])+'"'+' lon="'+str(point[1])+'">\n')
 			final_file.write('\t\t<ele>'+str(elevation_points[points_counter])+'</ele>\n')
-#			final_file.write('\t\t<time>'+str(time_points[points_counter])+'</time>\n')
+			final_file.write('\t\t<time>'+datetime.datetime.utcfromtimestamp(int(started_unix_time)+int(time_points[points_counter])).strftime('%Y-%m-%dT%H:%M:%SZ')+'</time>\n')
 			final_file.write('\t</trkpt>\n')
 		points_counter += 1
 
 
-	 final_file.write('</trkseg>\n')
-	 final_file.write('</trk>\n')
-	 final_file.write(FOOT_FILE)
-  	 final_file.close()
- 	 print("Info: Activity {0} = {1} points in track. file: {2}".format(str(activity_id),len(points),output_file))
+	final_file.write('</trkseg>\n')
+	final_file.write('</trk>\n')
+	final_file.write(FOOT_FILE)
+	final_file.close()
+	print("Info: Activity {0} = {1} points in track. file: {2}".format(str(activity_id),len(points),output_file))
 
 def login(login_username, login_password):
 	# Start a session so we can have persistant cookies
 	global LOGIN_SESSION
 	session = requests.session()
 	r = session.get(STRAVA_URL_LOGIN)
-	indice_final_linea =r.text.find('name=\"csrf-token\"')
-	indice_inicio_linea=r.text[1:indice_final_linea].rfind('<meta')
-	index1=r.text[indice_inicio_linea:indice_final_linea].find('\"')
-	authenticity_token = r.text[index1+indice_inicio_linea:indice_final_linea].strip().strip('\"')
+	soup = BeautifulSoup(r.content, 'html.parser')
 
+	get_details = soup.find('input', attrs={'name':'authenticity_token'})
+	authenticity_token = get_details.attrs.get('value')
+
+	#debug 
+	if args.verbose:
+		print('LOGIN TOKEN:'+authenticity_token)
+	
 	# This is the form data that the page sends when logging in
 	login_data = {
 		'email': login_username,
@@ -103,7 +110,7 @@ def login(login_username, login_password):
 	
 if __name__ == '__main__':
 
- 	parser = argparse.ArgumentParser(description='Download GPS Traces from Strava.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser = argparse.ArgumentParser(description='Download GPS Traces from Strava.',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('-a','--activity' , metavar='ID_Number',type=int, help='ID of activity to download')
 	parser.add_argument('-ai','--activityinterval' , nargs=2,metavar=('IDstart', 'IDend'),type=int,default=('None','None'), help='A interval of activities. auto outputname, not compatible with output name file option')
 	parser.add_argument('-o','--output'   , metavar='output.gpx',default=DEFAULT_OUTPUT_FILE, help='name of GPX file output.')
@@ -111,7 +118,7 @@ if __name__ == '__main__':
 	parser.add_argument('-v','--verbose', help='increase output verbosity', action='store_true')
 	args = parser.parse_args()
 
-        login_username=args.login[0]
+	login_username=args.login[0]
 	login_password=args.login[1]
 	output_file=args.output
 	login_ok=0
@@ -125,11 +132,11 @@ if __name__ == '__main__':
 	if(str(args.activity) != 'None'):
 		activityinterval_start = args.activity
 		activityinterval_stop =  args.activity
-        else:
-	        if(str(args.activityinterval[0]) != 'None'):
-		        print("WARNING: Download a lot of activities is activated. USE this option carefully.")	
-		        activityinterval_start = args.activityinterval[0]
-	                activityinterval_stop  = args.activityinterval[1]	
+	else:
+		if(str(args.activityinterval[0]) != 'None'):
+			print("WARNING: Download a lot of activities is activated. USE this option carefully.")	
+			activityinterval_start = args.activityinterval[0]
+			activityinterval_stop  = args.activityinterval[1]	
 
 	if login_username != 'None':
 		if login(str(login_username),str(login_password)):
@@ -139,7 +146,7 @@ if __name__ == '__main__':
 			print("Warning: LOGIN FAIL!")
 			login_ok=0
 
-        else:
+	else:
 		print("Warning: USER NOT LOGIN, without login only we get traces in a very low quality.")
 #    		user_filter = USER_FILTER
 		login_ok=0
@@ -148,30 +155,55 @@ if __name__ == '__main__':
 	session = LOGIN_SESSION 
 
 	for activity_id_temp in range (activityinterval_start, activityinterval_stop+1):
-	    try:
-		if(str(args.activityinterval[0]) != 'None'):
-			output_file = str(activity_id_temp)+".gpx"
+		try:
+			if(str(args.activityinterval[0]) != 'None'):
+				output_file = str(activity_id_temp)+".gpx"
 
-#		params = urllib.urlencode([('streams[]', 'latlng'), ('streams[]', 'altitude'), ('streams[]', 'distance'), ('streams[]', 'time')])
-		params = urllib.urlencode([('streams[]', 'latlng'), ('streams[]', 'altitude'), ('streams[]', 'time')])
+			params = urllib.parse.urlencode([('streams[]', 'latlng'), ('streams[]', 'altitude'), ('streams[]', 'time')])
 
-		if login_ok > 0:			
-			query = session.get(STRAVA_PATH_STREAM +str(activity_id_temp)+'?'+ params).text
-			#debug 
-			if args.verbose:
-			   print "url:"+STRAVA_PATH_STREAM +str(activity_id_temp)+'?'+ params
-		else:
-			query = urllib2.urlopen(STRAVA_PATH_STREAM +str(activity_id_temp)+'?'+ params).read()
-			#debug 
-			if args.verbose:
-			   print "url:"+STRAVA_PATH_STREAM +str(activity_id_temp)+'?'+ params
+			started_unix_time = 0
 
-		query = json.loads(query)
-		if 'latlng' in query.keys():
-			save_as_gpx(activity_id_temp, query['latlng'], query['altitude'], query['time'], output_file)
-		else:
-			 print("Info: No data for Activity {0}", format(activity_id_temp))
-	    except (KeyboardInterrupt, SystemExit):
-                sys.exit()
-            except:
-                print "Info: Can not get data for "+format(activity_id_temp)
+			if login_ok > 0:
+				#debug
+				if args.verbose:
+					print('getting track data:'+str(STRAVA_PATH_STREAM)+str(activity_id_temp)+'?'+params)
+
+				query = session.get(STRAVA_PATH_STREAM +str(activity_id_temp)+'?'+ params).text
+				
+				#debug
+				if args.verbose:
+					print('getting starting date:'+str(STRAVA_ACTIVITIES_SESSION)+str(activity_id_temp))
+
+				r = session.get(STRAVA_ACTIVITIES_SESSION +str(activity_id_temp))
+				indice_inicio_linea =r.text.find('startDateLocal:')
+				indice_fin_linea=r.text[indice_inicio_linea:(indice_inicio_linea+100)].find(',\n')
+				started_date=r.text[indice_inicio_linea+16:indice_inicio_linea+indice_fin_linea]
+				
+				#debug
+				if args.verbose:
+					print('startDateLocal:'+str(started_date))
+
+				started_unix_time = started_date
+
+			else:
+				#debug
+				if args.verbose:
+					print('getting track data:'+str(STRAVA_PATH_STREAM)+str(activity_id_temp)+'?'+params)
+
+				http = urllib3.PoolManager()
+				query = http.request('GET',STRAVA_PATH_STREAM +str(activity_id_temp)+'?'+ params)
+				query = query.data.decode('utf-8')
+				print("Warning: I can't get started date, so 1970 is setted, please check the file.")
+
+			query = json.loads(query)
+			if 'latlng' in query.keys():
+				save_as_gpx(activity_id_temp, query['latlng'], query['altitude'], query['time'],started_unix_time, output_file)
+			else:
+				print("Info: No data for Activity {0}", format(activity_id_temp))
+
+			
+		except (KeyboardInterrupt, SystemExit):
+			sys.exit()
+		except Exception:
+			print("Info: Can not get data for "+format(activity_id_temp))
+			traceback.print_exc()
